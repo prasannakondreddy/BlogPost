fc86fbeb9b824ef6e0e032d564622b01256a8375
prasannakondreddy <saiprasannakondreddy99@gmail.com>
2021-07-25 12:30:42 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const articleRouter=require("./routes/articles");
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    const articles=[{
+        title:"test article",
+        createdDate:new Date(),
+        text:"test data"
+    }]
+    res.render("index",{articles:articles});
+});
+
+app.use("/articles",articleRouter);
+
+app.listen(5000);
