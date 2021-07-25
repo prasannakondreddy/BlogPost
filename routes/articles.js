c27c4d6b5725a0e659a4e21a43c3f114161150b2
prasannakondreddy <saiprasannakondreddy99@gmail.com>
2021-07-25 16:40:14 +0530

    added create routes

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,33 @@
 const express=require("express");
+const article = require("./../models/article");
 const router=express.Router();
+const Article=require('./../models/article')
 
 router.get("/new",(req,res)=>{
-    res.render('articles/new');
+    res.render('articles/new',{article:new Article()});
 })
 
-router.post("/",(req,res)=>{
-    
+router.get("/:id",async(req,res)=>{
+    const article = await Article.findById(req.params.id)
+    if(article==null) res.redirect("/")
+    res.render("articles/show",{article:article})
 })
+
+router.post("/",async(req,res)=>{
+    let article=new Article({
+        title:req.body.title,
+        description:req.body.des,
+        markdown:req.body.markdown
+    })
+    try{
+        article=await article.save();
+        res.redirect(`/articles/${article.id}`)
+        }
+        catch(err){
+            res.render("articles/new",{article:article})
+        }
+})
+
 
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const Article = require("./models/article");
 const articleRouter=require("./routes/articles");
+
+mongoose.connect('mongodb://localhost/blog',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    
+})
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({
@@ -9,12 +16,8 @@ app.use(express.urlencoded({
 }));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-    const articles=[{
-        title:"test article",
-        createdDate:new Date(),
-        text:"test data"
-    }]
+app.get("/",async (req,res)=>{
+    const articles = await Article.find().sort({createdDate:'desc'});
     res.render("articles/index",{articles:articles});
 });
 
